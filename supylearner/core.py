98913cb5da7ff857306c09500792ee1086053a1e
ccotter <cotterman@gmail.com>
2015-06-14 23:12:53 -0700

    hard code to deal with ML algorithms that return predict_proba in different order

--- supylearner/core.py
+++ supylearner/core.py
@@ -336,7 +336,12 @@ class SuperLearner(BaseEstimator):
             pred=est.predict(X)
         if self.loss == 'nloglik':
             if hasattr(est, "predict_proba"):
-                pred=est.predict_proba(X)[:, 1]
+                #when "The columns correspond to the classes in sorted order, 
+                    #as they appear in the attribute classes_" -- LDA, svm.SVC
+                if est.__class__.__name__ == "SVC" or est.__class__.__name__ == "LDA":
+                    pred=est.predict_proba(X)[:, 0]
+                else:
+                    pred=est.predict_proba(X)[:, 1] 
             else:
                 pred=est.predict(X)
                 if pred.min() < 0 or pred.max() > 1:
